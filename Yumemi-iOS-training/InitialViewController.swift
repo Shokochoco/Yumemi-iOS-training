841b8f165986204a5781ea6cae4257260d8338bf
Shokochoco <shsuzu1992@gmail.com>
2022-02-15 01:31:14 +0900

    After Review1

--- Yumemi-iOS-training/InitialViewController.swift
+++ Yumemi-iOS-training/InitialViewController.swift
@@ -4,18 +4,10 @@ import UIKit
 class InitialViewController: UIViewController {
 
     override func viewWillLayoutSubviews() {
-        moveToViewController()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        moveToViewController()
-    }
-
-    func moveToViewController() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "viewController")
         vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: true, completion: nil)
     }
-
+    
 }
--- Yumemi-iOS-training/ViewController.swift
+++ Yumemi-iOS-training/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     let redLabel = UILabel()
     let closeButton = UIButton()
     let reloadButton = UIButton()
+    var closeBtnFlag: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -145,9 +146,18 @@ class ViewController: UIViewController {
 
     func closeButtonAction() {
         let action = UIAction { _ in
+            self.closeBtnFlag = true
             self.dismiss(animated: true, completion: nil)
         }
         self.closeButton.addAction(action, for: .touchUpInside)
+    }
+    // AlertControllerを閉じた時に下層Controllerまで道連れされるのを防ぐ
+    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        if self.presentedViewController != nil || self.closeBtnFlag {
+            // 上に重なってるControllerだけ閉じる
+            super.dismiss(animated: flag, completion: completion)
+            self.closeBtnFlag = false
+        }
     }
 
 }
